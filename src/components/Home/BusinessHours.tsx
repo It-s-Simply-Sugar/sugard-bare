d1ed40613db61f95b2acff34c594c09401ea62e8
Alanna Matieu <alannamatieu@rmatieu-mac.lan>
2023-05-06 17:20:10 -0400

    .

--- src/components/Home/BusinessHours.tsx
+++ src/components/Home/BusinessHours.tsx
@@ -14,20 +14,25 @@ const BusinessHours = () => {
     <div
       className="mx-auto py-20 bg-white grid place-items-center px-6 md:px-12"
       style={{ maxWidth: '1550px' }}>
-      <div className="w-full mb-16">
-        {hours.map((item, index) => (
-          <div
-            className="flex justify-between border-b mb-1"
-            style={{ borderColor: '#CCB6A0' }}
-            key={index}>
-            <div className="font-medium text-sm sm:text-md xl:text-lg">
-              <p>{item.day}</p>
+      <div className="w-full md:flex md:flex-row-reverse gap-x-4 mb-16">
+        <div className="w-full md:w-1/2 grid place-items-center bg-red-100">
+          <div>address</div>
+        </div>
+        <div className="w-full md:w-1/2">
+          {hours.map((item, index) => (
+            <div
+              className="flex justify-between border-b mb-1"
+              style={{ borderColor: '#CCB6A0' }}
+              key={index}>
+              <div className="font-medium text-sm sm:text-md xl:text-lg">
+                <p>{item.day}</p>
+              </div>
+              <div className="font-medium text-sm sm:text-md xl:text-lg">
+                <p>{item.hour}</p>
+              </div>
             </div>
-            <div className="font-medium text-sm sm:text-md xl:text-lg">
-              <p>{item.hour}</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3526.2867157320557!2d-82.28981732387929!3d27.893173476076566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c2d3476fbe30f3%3A0xa19b37b569911e82!2sIt%E2%80%99s%20Simply%20Sugar%2F%20Sugaring%20Hair%20Removal!5e0!3m2!1sen!2sus!4v1682977932807!5m2!1sen!2sus"
